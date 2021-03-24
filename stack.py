7a1f6e94d2a5c7f18282c355e97e3d051a34d86a
Julius Krahn <juliuskrahn@outlook.de>
2021-03-24 10:38:50 +0100

    Don't cache index.html...

--- stack.py
+++ stack.py
@@ -23,24 +23,6 @@ class Stack(cdk.core.Stack):
             website_index_document="index.html"
         )
 
-        s3deploy.BucketDeployment(
-            self,
-            "blog-deployment",
-            destination_bucket=bucket,
-            sources=[s3deploy.Source.asset("app/dist")],
-            retain_on_delete=False,
-            cache_control=[
-                s3deploy.CacheControl.set_public(),
-                s3deploy.CacheControl.max_age(cdk.core.Duration.days(2)),
-            ]
-        )
-
-        certificate = cm.Certificate.from_certificate_arn(
-            self,
-            "blog-domain-name-certificate",
-            "arn:aws:acm:us-east-1:473883619336:certificate/d5414905-09a8-43a1-a622-809971cffca0"
-        )
-
         cloudfront_distribution = cloudfront.Distribution(
             self,
             "blog-cloudfront-distribution",
@@ -51,7 +33,11 @@ class Stack(cdk.core.Stack):
                 cache_policy=cloudfront.CachePolicy.CACHING_OPTIMIZED,
             ),
             domain_names=["juliuskrahn.com"],
-            certificate=certificate,
+            certificate=cm.Certificate.from_certificate_arn(
+                self,
+                "blog-domain-name-certificate",
+                "arn:aws:acm:us-east-1:473883619336:certificate/d5414905-09a8-43a1-a622-809971cffca0"
+            ),
             error_responses=[
                 cloudfront.ErrorResponse(
                     http_status=404,
@@ -62,9 +48,38 @@ class Stack(cdk.core.Stack):
             price_class=cloudfront.PriceClass.PRICE_CLASS_100
         )
 
+        # TODO how does Cloudfront tell the client to cache locally?
+
         route53.ARecord(
             self,
             "blog-frontend-a-record",
             target=route53.RecordTarget.from_alias(route53_targets.CloudFrontTarget(cloudfront_distribution)),
             zone=route53.HostedZone.from_lookup(self, "blog-hosted-zone", domain_name="juliuskrahn.com")
+        )
+
+        # deploy index.html
+
+        s3deploy.BucketDeployment(
+            self,
+            "blog-deployment-index",
+            destination_bucket=bucket,
+            sources=[s3deploy.Source.asset("app/dist/index.html")],
+            retain_on_delete=False,
+            prune=False,  # -> keep files in destination that aren't present in source
+            cache_control=[s3deploy.CacheControl.no_cache]
+        )
+
+        # deploy everything else
+
+        s3deploy.BucketDeployment(
+            self,
+            "blog-deployment-app",
+            destination_bucket=bucket,
+            sources=[s3deploy.Source.asset("app/dist", exclude=["index.html"])],
+            retain_on_delete=False,
+            prune=False,  # -> keep files in destination that aren't present in source
+            cache_control=[
+                s3deploy.CacheControl.set_public(),
+                s3deploy.CacheControl.max_age(cdk.core.Duration.days(365)),
+            ]
         )
